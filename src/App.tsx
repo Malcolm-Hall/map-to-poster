eed509b9c7db03e79f9ab9a6b35a99162e5f0d3a
Malcolm Hall <malcolm.r.hall@outlook.com>
2026-02-03 14:55:45 +0200

    Add react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
 import { Button } from "@/components/ui/button";
+import { useQuery } from "@tanstack/react-query";
 
 export default function App() {
+  const { isPending, error, data, isFetching } = useQuery({
+    queryKey: ["repoData"],
+    queryFn: async () => {
+      const response = await fetch(
+        "https://api.github.com/repos/TanStack/query",
+      );
+      return await response.json();
+    },
+  });
+
+  if (isPending) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello, World</h1>
+    <div>
+      <h1 className="text-3xl font-bold underline">{data.full_name}</h1>
+      <p>{data.description}</p>
+      <strong>👀 {data.subscribers_count}</strong>{" "}
+      <strong>✨ {data.stargazers_count}</strong>{" "}
+      <strong>🍴 {data.forks_count}</strong>
+      <div>{isFetching ? "Updating..." : ""}</div>
       <Button>Click me</Button>
-    </>
+    </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-createRoot(document.getElementById('root')!).render(
+const HALF_DAY_MILLIS = 1000 * 60 * 60 * 12;
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: HALF_DAY_MILLIS,
+      gcTime: HALF_DAY_MILLIS,
+      refetchOnReconnect: false,
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+    },
+  },
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
-)
+);
