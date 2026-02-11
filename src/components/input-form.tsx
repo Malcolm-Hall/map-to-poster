61efd380e0ccb07d3b0bdb5cb708e3901da1a2cb
Malcolm Hall <malcolm.r.hall@outlook.com>
2026-02-11 14:24:04 +0200

    Add toggle for water feature generation

--- src/components/input-form.tsx
+++ src/components/input-form.tsx
@@ -1,7 +1,14 @@
-import { Field, FieldGroup, FieldLabel } from "@/components/ui/field";
+import {
+  Field,
+  FieldContent,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import type { GenerationConfig } from "@/models/generation";
+import { Checkbox } from "./ui/checkbox";
 
 type Props = {
   onSubmit: (config: GenerationConfig) => void;
@@ -10,13 +17,13 @@ type Props = {
 export default function InputForm({ onSubmit }: Props) {
   return (
     <form
-      className="max-w-sm"
+      className="m-4 max-w-sm"
       onSubmit={(e) => {
         e.preventDefault();
         onSubmit({
           city: e.target.city.value,
           country: e.target.country.value,
-          showWaterFeatures: false,
+          showWaterFeatures: e.target["water-features-enabled"].checked,
         });
       }}
     >
@@ -28,6 +35,15 @@ export default function InputForm({ onSubmit }: Props) {
         <Field>
           <FieldLabel htmlFor="country">Country</FieldLabel>
           <Input id="country" placeholder="USA" required />
+        </Field>
+        <Field orientation="horizontal">
+          <Checkbox id="water-features" name="water-features-enabled" />
+          <FieldContent>
+            <FieldLabel htmlFor="water-features">
+              Enable Water Features
+            </FieldLabel>
+            <FieldDescription>Experimental</FieldDescription>
+          </FieldContent>
         </Field>
         <Field orientation="horizontal">
           <Button type="submit">Generate</Button>
