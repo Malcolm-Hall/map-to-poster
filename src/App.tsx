6a81d70f9e78f99b3530f02c170f889d9de88b44
Malcolm Hall <malcolm.r.hall@outlook.com>
2026-02-18 14:16:45 +0200

    Add background gradient

--- src/App.tsx
+++ src/App.tsx
@@ -13,32 +13,36 @@ export default function App() {
   return (
     <>
       <Header />
-      <Hero />
-      <main id="generator" className="mx-auto max-w-7xl px-4 py-8">
-        <div className="grid place-items-center items-start gap-8 lg:grid-cols-5">
-          <div className="w-full max-w-lg lg:col-span-2">
-            <div className="w-full p-4">
-              <Card>
+      <div className="via-background bg-linear-to-r from-blue-50 to-blue-50">
+        <Hero />
+        <main id="generator" className="mx-auto max-w-7xl px-4 py-8">
+          <div className="grid place-items-center items-start gap-4 lg:grid-cols-5">
+            <div className="w-full max-w-lg lg:col-span-2">
+              <div className="w-full p-4">
+                <Card>
+                  <CardContent>
+                    <InputForm
+                      onSubmit={setConfig}
+                      onReset={() => setConfig(undefined)}
+                    />
+                  </CardContent>
+                </Card>
+              </div>
+            </div>
+            <div className="h-full w-full py-4 lg:col-span-3">
+              <Card className="h-full">
                 <CardContent>
-                  <InputForm
-                    onSubmit={setConfig}
-                    onReset={() => setConfig(undefined)}
-                  />
+                  {!config ? (
+                    <IndicatorInfo title="Generate a poster to preview it here" />
+                  ) : (
+                    <OutputPanel config={config} />
+                  )}
                 </CardContent>
               </Card>
             </div>
           </div>
-          <div className="h-full w-full p-4 lg:col-span-3">
-            <div className="h-full w-full rounded-2xl border p-4">
-              {!config ? (
-                <IndicatorInfo title="Generate a poster to preview it here" />
-              ) : (
-                <OutputPanel config={config} />
-              )}
-            </div>
-          </div>
-        </div>
-      </main>
+        </main>
+      </div>
     </>
   );
 }
